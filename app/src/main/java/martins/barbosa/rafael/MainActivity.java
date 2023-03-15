deba7d66fd5464ca5975737680719c6c0c2afd00
20201tiimi0250 <rafaelmartinscouto@gmail.com>
2023-03-15 12:46:57 -0300

    Código comentado

--- app/src/main/java/martins/barbosa/rafael/MainActivity.java
+++ app/src/main/java/martins/barbosa/rafael/MainActivity.java
@@ -14,14 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Armazenando o botão de MainActivity.xml em uma variavel
         Button btnEnviar = findViewById(R.id.btnEnviar);
+        //Definindo um ouvidor de cliques(evento) para o botão que acabei de pegar
         btnEnviar.setOnClickListener(new View.OnClickListener() {
+            //Metodo que sera acionado ou clicar no botão
             @Override
             public void onClick(View view) {
+                //Armazenando o editText de MainActivity.xml em uma variavel
                 EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
+                //Pegando o texto de editText e armazenando em uma variavel do tipo str
                 String texto = etDigiteAqui.getText().toString();
+                //Criando a intenção de viajar de MainActivity para ProximaActivity
                 Intent intent = new Intent(MainActivity.this, ProximaActivity.class);
+                //Colocando no dicionario que acompanhara a intent a variavel que contem o texto com a chave "texto"
                 intent.putExtra("texto", texto);
+                //Executando a intent
                 startActivity(intent);
             }
         });
--- app/src/main/java/martins/barbosa/rafael/ProximaActivity.java
+++ app/src/main/java/martins/barbosa/rafael/ProximaActivity.java
@@ -12,9 +12,13 @@ public class ProximaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_proxima);
+        //Pegando a intent que criou a tela e armazenando em uma variavel
         Intent intent = getIntent();
+        //Pegando do dicionario da intent o valor com chave "texto" e armazenando em uma variavel
         String texto = intent.getStringExtra("texto");
+        //Armazenando o textView de ProximaActivity.xml em uma variavel
         TextView tvExibir = findViewById(R.id.tvExibir);
+        //Definindo o texto da textView com o valor do texto do dicionario
         tvExibir.setText(texto);
     }
 }
